970a7bbd9571cc14458aeddfe35b32127688301f
dayrton <david@stormtrooper>
2024-01-03 19:06:40 -0700

    bundle

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704224038';
+const cacheName = 'chirpy-1704333966';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
